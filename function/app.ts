7195c59114f3a426ec283597503c7fc345673a26
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-25 18:42:40 +0700

    feat(declare_var): function  ts

---
+++ function/app.ts
@@ -0,0 +1,16 @@
+function add(a: number, b:number) {
+  return a + b;
+}
+
+function evenOrOdd(a: number): string {
+  return a % 2 == 0 ? "Genap" : "Ganjil"
+}
+
+let checkNumber = (a: number) => {return a % 2 == 0 ? "Genap" :  "Ganjil" }
+
+console.log(add(10, 29))
+
+console.log(evenOrOdd(10))
+
+console.log(checkNumber(33))
+
