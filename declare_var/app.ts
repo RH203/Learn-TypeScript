ce3e78be32cf17976d1181cb5eb8381b94d1d17f
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-25 15:46:35 +0700

    feat(declare_var): how to declare variable ts

---
+++ declare_var/app.ts
@@ -0,0 +1,24 @@
+let nameMahasiwa: string | null;
+
+nameMahasiwa = "Makoto";
+console.log(nameMahasiwa);
+nameMahasiwa = null;
+console.log(nameMahasiwa);
+
+// nameMahasiwa = 29; this is not allowed in TypeScript
+console.log(nameMahasiwa);
+
+let notSure: any;
+notSure = "Makoto"
+console.log(notSure)
+notSure = 20
+console.log(notSure)
+notSure = true
+console.log(notSure)
+
+
+/*
+  ? Declare Variable
+  * let = This variable may change later
+  ! const = This variable may not change later
+*/
